fb2fe52b28083b84bd876956aa320056b00cd562
Wenddy Romero <romerowmariel@gmail.com>
2026-06-26 16:36:31 -0400

    Ejercicio 5: fetch-usuario-api

--- web-desarrollo/ejercicio5.js
+++ web-desarrollo/ejercicio5.js
@@ -2,14 +2,20 @@
 // Responde con un objeto que incluye, entre otros campos: { id: 1, name: "Leanne Graham", ... }
 
 async function obtenerUsuario() {
-  // TODO: Usa fetch para obtener los datos del usuario con id 1
-  const response = await /* tu código aquí */;
+  try {
+    // TODO: Usa fetch para obtener los datos del usuario con id 1
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/users/1",
+    );
 
-  // TODO: Convierte la respuesta a JSON
-  const data = await /* tu código aquí */;
+    // TODO: Convierte la respuesta a JSON
+    const data = await response.json();
 
-  // TODO: Imprime en consola: "Usuario: " seguido del nombre del usuario
-  /* tu código aquí */
+    // TODO: Imprime en consola: "Usuario: " seguido del nombre del usuario
+    console.log("Usuario: " + data.name);
+  } catch (error) {
+    console.log("Error al obtener usuario:", error);
+  }
 }
 
 obtenerUsuario();
